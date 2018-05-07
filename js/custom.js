be23e8beaa74df76a322368225b63d66e1954667
Márcio <marciodepaula133@gmail.com>
2018-05-06 23:45:20 -0300

    Social Bar

--- js/custom.js
+++ js/custom.js
@@ -50,6 +50,19 @@ $(document).on('mousewheel ', function(e) {
 
 });
 
+$( "#linkedin-link" ).click(function() {
+    window.open('https://www.linkedin.com/in/marcio-de-paula-monte');
+
+  });
+$( "#github-link" ).click(function() {
+    window.open('https://github.com/marcio133');
+
+  });
+$( "#email-link" ).click(function() {
+    window.open('mailto:marciodepaula133@gmail.com');
+
+  });
+
 
 function scrollTo($el) {
     $('html,body').animate({
